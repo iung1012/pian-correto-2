583ba793d01f27219b1ee7b383a19f66734d03c3
mateus <fadebrasil@mail.com>
2025-12-04 22:05:44 -0300

    fix: mover imagens IMG_3137.PNG e IMG_3138.PNG para pasta public

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t8nlmhbdnbo"
+    "revision": "0.14cf6ng84b8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
